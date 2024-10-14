9d312e1fb7e02e28e376b35037e06af162e59a2c
johnm <johnm@DESKTOP-FE6KK2G>
2024-10-14 23:01:01 +0100

    I was working on a functional calculator

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Variable names should be short yet meaningful.
+//The choice of a variable name should be mnemonic- that is,
+//designed to indicate to the casual observer the intent of its use.
+
+//This integer variable stores he first number
+//Ask the user to inout the first number
+Console.WriteLine("Type in the first number followed by the Enter key");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Type in the second number followed by the Enter key");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+//Perform the calculation
+int result = firstNumber + secondNumber;
+
+//Output the answer to the console
+Console.WriteLine("Adding {0} and {1} give the answer {2}", firstNumber, secondNumber, result);
+
+
+
+
